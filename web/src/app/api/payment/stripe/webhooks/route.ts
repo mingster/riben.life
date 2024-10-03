1cc402512d4894c54b33251ba1e27f5923236d25
mingster <mingster@users.noreply.github.com>
2024-10-03 11:12:38 +0800

    WIP

--- web/src/app/api/payment/stripe/webhooks/route.ts
+++ web/src/app/api/payment/stripe/webhooks/route.ts
@@ -1,6 +1,6 @@
-import getStripe from "@/lib/stripe/client";
+import { stripe } from "@/lib/stripe/config";
 import { NextResponse } from "next/server";
-import Stripe from "stripe";
+import type Stripe from "stripe";
 
 const relevantEvents = new Set([
   'product.created',
@@ -15,23 +15,8 @@ const relevantEvents = new Set([
   'customer.subscription.deleted'
 ]);
 
-export const stripe = new Stripe(
-  process.env.STRIPE_SECRET_KEY_LIVE ?? process.env.STRIPE_SECRET_KEY ?? '',
-  {
-    // https://github.com/stripe/stripe-node#configuration
-    // https://stripe.com/docs/api/versioning
-    // @ts-ignore
-    apiVersion: null,
-    // Register this as an official Stripe plugin.
-    // https://stripe.com/docs/building-plugins#setappinfo
-    appInfo: {
-      name: 'Next.js Subscription Starter',
-      version: '0.0.0',
-      url: 'https://github.com/vercel/nextjs-subscription-payments'
-    }
-  }
-);
-//create stripe payment intent
+
+// stripe webhook handler
 export async function POST(
   req: Request,
 ) {
