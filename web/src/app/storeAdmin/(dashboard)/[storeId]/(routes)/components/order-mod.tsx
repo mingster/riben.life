d175cf0e8b48f471609eff9049827279f4313e62
mingster <mingster@users.noreply.github.com>
2024-10-15 15:13:17 +0800

    WIP

--- web/src/app/storeAdmin/(dashboard)/[storeId]/(routes)/components/order-mod.tsx
+++ web/src/app/storeAdmin/(dashboard)/[storeId]/(routes)/components/order-mod.tsx
@@ -6,7 +6,7 @@ import type { ItemOption } from "@/hooks/use-cart";
 
 import { Button } from "@/components/ui/button";
 import type { Product, ProductOption, Store, StoreOrder } from "@/types";
-import type { ProductOptionSelections, StoreTables } from "@prisma/client";
+import type { ProductOptionSelections, StoreTables, StoreShipMethodMapping } from "@prisma/client";
 import { useForm } from "react-hook-form";
 
 import { useTranslation } from "@/app/i18n/client";
@@ -58,6 +58,50 @@ export const ModifiyOrderDialog: React.FC<props> = ({ store, order }) => {
   const { lng } = useI18n();
   const { t } = useTranslation(lng, "storeAdmin");
 
+
+  const formSchema = z.object({
+    tableId: z.string().min(1),
+    orderNum: z.number().optional(),
+    paymentMethodId: z.string().optional(),
+    shippingMethodId: z.string().optional(),
+  });
+
+  type formValues = z.infer<typeof formSchema>;
+
+  const defaultValues = order
+    ? {
+      ...order,
+    }
+    : {};
+
+  //console.log('defaultValues: ' + JSON.stringify(defaultValues));
+  const form = useForm<formValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues,
+  });
+
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+    watch,
+    clearErrors,
+  } = useForm<formValues>();
+
+
+  const onSubmit = async (data: formValues) => {
+    setLoading(true);
+
+
+    console.log('formValues', JSON.stringify(data));
+    toast({
+      title: '訂單更新了',
+      description: "",
+      variant: "success",
+    });
+    setLoading(false);
+  }
+
   //console.log('StorePaymentMethods', JSON.stringify(store.StorePaymentMethods));
 
   //const params = useParams();
@@ -72,6 +116,28 @@ export const ModifiyOrderDialog: React.FC<props> = ({ store, order }) => {
     setOpen(false);
   };
 
+  const handleTableChange = (fieldName: string, selectedVal: string) => {
+    //console.log("fieldName", fieldName, selectedVal);
+    //console.log("selected", selected);
+
+    /*
+    const base_price = Number(product.price);
+    let selected = null;
+    if (fieldName === "option1") {
+      selected = option1_items.find((item) => item.name === selectedVal);
+    } else if (fieldName === "option4") {
+      selected = option4_items.find((item) => item.name === selectedVal);
+      console.log("selected", selected);
+    }
+
+    if (selected) {
+      const p = base_price + (selected?.price ?? 0) + checkedTotal;
+      setUnitPrice(p);
+      setTotal(quantity * p);
+    }
+    */
+  };
+
   return (
     <>
       <Dialog open={open} onOpenChange={setOpen}>
@@ -84,36 +150,110 @@ export const ModifiyOrderDialog: React.FC<props> = ({ store, order }) => {
           <div className="flex h-full flex-col">
             <DialogHeader className="border-b p-4">
               <DialogTitle>修改訂單</DialogTitle>
-              <DialogDescription></DialogDescription>
+              <DialogDescription>&nbsp;</DialogDescription>
             </DialogHeader>
 
             <div className="flex-1 overflow-auto p-4">
-              <div className="pb-5">
-                {store.StoreShippingMethods.map((method) => (
-                  <div key={method.id}>{method.ShippingMethod.name}</div>
-                ))}
 
-                <StoreTableCombobox
-                  storeId={store.id}
-                  defaultValue={order.tableId || ""}
-                  disabled={loading}
-                />
-              </div>
+              <Form {...form}>
+                <form
+                  onSubmit={form.handleSubmit(onSubmit)}
+                  className="w-full space-y-1"
+                >
+                  <div className="pb-5 flex items-center gap-5">
+                    <FormField
+                      control={form.control}
+                      name="shippingMethodId"
+                      render={({ field }) => (
+                        <FormItem className="flex items-center">
+                          <FormControl>
+                            <RadioGroup
+                              //onValueChange={(val) => handleTableChange(field.name, val)}
+                              defaultValue={field.value}
+                              className="flex items-center space-x-1 space-y-0"
+                            >
+                              {store.StoreShippingMethods.map((item) => (
+                                <div
+                                  key={item.methodId}
+                                  className="flex items-center"
+                                >
+                                  <FormItem className="flex items-center space-x-1 space-y-0">
+                                    <FormControl>
+                                      <RadioGroupItem value={item.methodId} />
+                                    </FormControl>
+                                    <FormLabel className="font-normal">
+                                      {item.ShippingMethod.name}
+                                    </FormLabel>
+                                  </FormItem>
+                                </div>
+                              ))}
+                            </RadioGroup>
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                    <FormField
+                      control={form.control}
+                      name="tableId"
+                      render={({ field }) => (
+                        <FormItem className="flex items-center space-x-1 space-y-0">
+                          <FormLabel className="text-nowrap">桌號</FormLabel>
+                          <StoreTableCombobox
+                            disabled={loading}
+                            storeId={store.id}
+                            onValueChange={field.onChange}
+                            defaultValue={field.value}
+                          />
+                        </FormItem>
+                      )}
+                    />
 
-              <div className="pb-5">改付款方式</div>
+                  </div>
 
-              <div className="pb-5">改menu</div>
+                  <div className="pb-5">改付款方式</div>
+
+
+
+                  <DialogFooter className="w-full pt-2 pb-2">
+
+                    <Button
+                      disabled={loading}
+                      className="disabled:opacity-25"
+                      type="submit"
+                    >
+                      {t("Save")}
+                    </Button>
+
+                    <Button
+                      type="button"
+                      variant="outline"
+                      onClick={() => {
+                        clearErrors(); setOpen(false);
+                      }}
+                      className="ml-2 disabled:opacity-25"
+                    >
+                      {t("Cancel")}
+                    </Button>
+
+                    <Button
+                      className="text-xs"
+                      variant={"destructive"}
+                      onClick={onCancel}
+                    >
+                      刪單
+                    </Button>
+                  </DialogFooter>
+
+
+                  <div className="pb-5">改menu</div>
+
+
+                </form>
+              </Form>
             </div>
+
           </div>
-          <DialogFooter className="w-full pt-2 pb-2">
-            <Button
-              className="text-xs"
-              variant={"destructive"}
-              onClick={onCancel}
-            >
-              刪單
-            </Button>
-          </DialogFooter>
         </DialogContent>
       </Dialog>
     </>
--- web/src/app/storeAdmin/(dashboard)/[storeId]/(routes)/components/store-table-combobox.tsx
+++ web/src/app/storeAdmin/(dashboard)/[storeId]/(routes)/components/store-table-combobox.tsx
@@ -48,6 +48,14 @@ export const StoreTableCombobox = ({
     tables = data as StoreTables[];
   }
 
+  /*
+  tables.push({
+    id: "",
+    tableName: "---",
+    storeId: storeId, capacity: 0
+  });
+  */
+
   const [open, setOpen] = React.useState(false);
 
   // combox is buggy if default value is empty....
@@ -69,7 +77,7 @@ export const StoreTableCombobox = ({
           <PopoverTrigger asChild>
             <Button
               variant="outline"
-              className="flex h-9 w-full items-center justify-between whitespace-nowrap rounded-md border border-input bg-transparent px-3 py-2 text-sm shadow-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-1 focus:ring-ring disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1"
+              className=""
               disabled={disabled}
               {...props}
             >
