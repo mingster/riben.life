337d9e4c9c00de5b219eeab3d53dc9b874ecbd8c
mingster <mingster@users.noreply.github.com>
2025-02-03 10:51:12 +0800

    WIP

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -16,39 +16,39 @@ import { CookiesProvider } from "next-client-cookies/server";
 
 const title = "riben.life 利便生活";
 export const metadata: Metadata = {
-	title: {
-		template: `%s | ${title}`,
-		default: title, // a default is required when creating a template
-	},
-	keywords: ["掃碼點餐", "線上點餐", "QR code 點餐"],
-	authors: [{ name: "利便生活", url: "https://riben.life" }],
-	creator: "riben.life",
-	publisher: "riben.life",
+  title: {
+    template: `%s | ${title}`,
+    default: title, // a default is required when creating a template
+  },
+  keywords: ["掃碼點餐", "線上點餐", "QR code 點餐"],
+  authors: [{ name: "利便生活", url: "https://riben.life" }],
+  creator: "riben.life",
+  publisher: "riben.life",
 
-	openGraph: {
-		title: title,
-		description:
-			"導入線上點餐系統，讓您的銷售流程更順暢。沒有前置費用、 增加營業額、 客戶無需等待、 只需手機或平版電腦，您就可以開始使用系統。",
-		url: "https://riben.life",
-		siteName: title,
-		type: "website",
-	},
-	robots: {
-		index: false,
-		follow: true,
-		nocache: true,
-		googleBot: {
-			index: true,
-			follow: false,
-			noimageindex: true,
-			"max-video-preview": -1,
-			"max-image-preview": "large",
-			"max-snippet": -1,
-		},
-	},
-	manifest: "/favicons/site.webmanifest",
-	applicationName: title,
-	/*
+  openGraph: {
+    title: title,
+    description:
+      "導入線上點餐系統，讓您的銷售流程更順暢。沒有前置費用、 增加營業額、 客戶無需等待、 只需手機或平版電腦，您就可以開始使用系統。",
+    url: "https://riben.life",
+    siteName: title,
+    type: "website",
+  },
+  robots: {
+    index: false,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: false,
+      noimageindex: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+  manifest: "/favicons/site.webmanifest",
+  applicationName: title,
+  /*
   appleWebApp: {
     title: title,
     capable: true,
@@ -59,75 +59,75 @@ export const metadata: Metadata = {
     { media: '(prefers-color-scheme: light)', color: '#f8fafc' },
   ],
   */
-	icons: {
-		icon: "/favicons/favicon-16x16.png",
-		shortcut: "/favicons/favicon-32x32.png",
-		apple: [
-			{ url: "/favicons/apple-touch-icon.png" },
-			{
-				url: "/favicons/apple-touch-icon.png",
-				sizes: "180x180",
-				type: "image/png",
-			},
-		],
-		other: {
-			rel: "apple-touch-icon-precomposed",
-			url: "/favicons/apple-touch-icon.png",
-		},
-	},
-	verification: {
-		google: "google",
-		yandex: "yandex",
-		yahoo: "yahoo",
-		other: {
-			"google-site-verification": "google-site-verification",
-		},
-	},
+  icons: {
+    icon: "/favicons/favicon-16x16.png",
+    shortcut: "/favicons/favicon-32x32.png",
+    apple: [
+      { url: "/favicons/apple-touch-icon.png" },
+      {
+        url: "/favicons/apple-touch-icon.png",
+        sizes: "180x180",
+        type: "image/png",
+      },
+    ],
+    other: {
+      rel: "apple-touch-icon-precomposed",
+      url: "/favicons/apple-touch-icon.png",
+    },
+  },
+  verification: {
+    google: "google",
+    yandex: "yandex",
+    yahoo: "yahoo",
+    other: {
+      "google-site-verification": "google-site-verification",
+    },
+  },
 };
 
 export default async function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	//const session = (await getServerSession(authOptions)) as Session;
-	const session = (await GetSession()) as Session;
+  //const session = (await getServerSession(authOptions)) as Session;
+  const session = (await GetSession()) as Session;
 
-	return (
-		<SessionWrapper session={session}>
-			<html
-				lang="en"
-				className="dark [--scroll-mt:9.875rem] lg:[--scroll-mt:6.3125rem] [scrollbar-gutter:stable]"
-			>
-				<head>
-					<link rel="preconnect" href="https://fonts.googleapis.com" />
-					<link
-						rel="preconnect"
-						href="https://fonts.gstatic.com"
-						crossOrigin=""
-					/>
-					<link
-						href="https://fonts.googleapis.com/css2?family=Host_Grotesk:wght@400;700&family=Noto+Sans+TC:wght@400;700&family=Rubik:wght@500;700&display=swap"
-						rel="stylesheet"
-					/>
-				</head>
+  return (
+    <SessionWrapper session={session}>
+      <html
+        lang="en"
+        className="dark [--scroll-mt:9.875rem] lg:[--scroll-mt:6.3125rem] [scrollbar-gutter:stable]"
+      >
+        <head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin=""
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Host_Grotesk:wght@400;700&family=Noto+Sans+TC:wght@400;700&family=Rubik:wght@500;700&display=swap"
+            rel="stylesheet"
+          />
+        </head>
 
-				<body className="antialiased text-primary-400 dark:text-primary-400 min-h-screen">
-					<NextThemeProvider attribute="class" defaultTheme="dark" enableSystem>
-						<CartProvider>
-							<CookiesProvider>
-								<I18nProvider>{children}</I18nProvider>
-							</CookiesProvider>
-						</CartProvider>
-					</NextThemeProvider>
-					<Toaster />
-					{env.NODE_ENV === "production" && (
-						<>
-							<SpeedInsights /> <Analytics />
-						</>
-					)}
-				</body>
-			</html>
-		</SessionWrapper>
-	);
+        <body className="antialiased text-primary-400 dark:text-primary-400 min-h-screen">
+          <NextThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+            <CartProvider>
+              <CookiesProvider>
+                <I18nProvider>{children}</I18nProvider>
+              </CookiesProvider>
+            </CartProvider>
+          </NextThemeProvider>
+          <Toaster />
+          {env.NODE_ENV === "production" && (
+            <>
+              <SpeedInsights /> <Analytics />
+            </>
+          )}
+        </body>
+      </html>
+    </SessionWrapper>
+  );
 }
