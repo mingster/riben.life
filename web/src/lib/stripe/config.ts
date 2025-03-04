a380c64ecb355d1da65ef7a4229f87e69b28d849
mingster <mingster@users.noreply.github.com>
2025-03-04 13:17:12 +0800

    FEAT: upgrade stripe

--- web/src/lib/stripe/config.ts
+++ web/src/lib/stripe/config.ts
@@ -3,7 +3,7 @@ import Stripe from "stripe";
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY ?? "", {
 	// https://github.com/stripe/stripe-node#configuration
 	// https://stripe.com/docs/api/versioning
-	apiVersion: "2025-01-27.acacia",
+	apiVersion: "2025-02-24.acacia",
 	typescript: true,
 
 	// Register this as an official Stripe plugin.
