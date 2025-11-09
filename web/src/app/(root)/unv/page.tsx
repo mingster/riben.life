07a8cafe48aab2871e056ae56061d4d90f450e2b
mingster <mingster@users.noreply.github.com>
2025-11-09 21:24:39 +0800

    fix:🐛 (unv should be anonymous):

--- web/src/app/(root)/unv/page.tsx
+++ web/src/app/(root)/unv/page.tsx
@@ -1,11 +1,8 @@
 import { Loader } from "@/components/loader";
 import { Suspense } from "react";
 import { UniversalHomeContent } from "./components/universal-home-content";
-import { requireAuth } from "@/lib/auth-utils";
 
 export default async function GlobalHome() {
-	const session = await requireAuth();
-
 	return (
 		<>
 			<Suspense fallback={<Loader />}>
