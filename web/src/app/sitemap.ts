adeb3fc6a5bb252771e53ee06263ea43a5b06336
mingster <mingster@users.noreply.github.com>
2025-11-16 20:15:32 +0800

    feat:🎸 (robots and sitemap):

---
+++ web/src/app/sitemap.ts
@@ -0,0 +1,48 @@
+import { sqlClient } from "@/lib/prismadb";
+import type { MetadataRoute } from "next";
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+	const baseUrl: string =
+		process.env.NEXT_PUBLIC_BASE_URL ?? "https://riben.life";
+
+	// list all stores in the database
+	const stores = await sqlClient.store.findMany({
+		select: {
+			id: true,
+			name: true,
+			createdAt: true,
+		},
+	});
+
+	const staticRoutes: MetadataRoute.Sitemap = stores.map((store) => ({
+		url: `${baseUrl}/${store.id}`,
+		lastModified: store.createdAt,
+		changeFrequency: "monthly",
+		priority: 0.8,
+	}));
+
+
+	staticRoutes.push({
+		url: `${baseUrl}/unv`,
+			lastModified: new Date(),
+				changeFrequency: "monthly",
+					priority: 0.8,
+	});
+	
+	staticRoutes.push({
+		url: `${baseUrl}/privacy`,
+			lastModified: new Date(),
+				changeFrequency: "monthly",
+					priority: 0.5,
+	});
+	
+	staticRoutes.push({
+		url: `${baseUrl}/terms`,
+			lastModified: new Date(),
+				changeFrequency: "monthly",
+					priority: 0.5,
+	});
+	
+	
+	return staticRoutes;
+}
